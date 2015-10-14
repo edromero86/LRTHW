63f1420b70ac46641b6e56a8b669cc32304b3ff8
Edgar Romero <edromero86@hotmail.com>
2015-10-14 14:27:29 -0500

    Adding ex2-9

---
+++ ex5.rb
@@ -0,0 +1,18 @@
+my_name = 'Edgar Romero'
+my_age = 29
+my_height = 59
+my_weight = 230
+my_eyes = 'brown'
+my_teeth = 'White'
+my_hair = 'brown'
+
+puts "Let's talk about #{my_name}."
+puts "He's #{my_height} inches tall."
+puts "He's #{my_weight} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth ae usualy #{my_teeth} depending on the coffee."
+
+# This is tricky, try to get it exactly right
+puts "If I add #{my_age}, #{my_height}, and #{my_weight}
+	I get #{my_age + my_height + my_weight}."
